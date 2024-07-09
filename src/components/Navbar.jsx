ff6658415c3ec07778e51d0ea6e95bb66505f652
AhmedYoussef2 <ahmed.youssef.m5@gmail.com>
2024-07-09 13:14:43 +0300

    menu icon is responsive now

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { FaBars } from "react-icons/fa";
+import { FaBars ,FaTimes} from "react-icons/fa";
 import "../styles/Navbar.css";
 
 const Navbar = () => {
@@ -12,12 +12,12 @@ const Navbar = () => {
 
     return (
         <nav className="navbar bg-black text-white p-4">
-            <div className="menu-icon" onClick={toggleMenu}><FaBars /></div>
+            <div className="menu-icon" onClick={toggleMenu}>{!isMenuOpen? <FaBars/>:<FaTimes/>}</div>
             <ul className={`menu-list flex text-center space-x-12 hover:space-x-14 font-sans list ${isMenuOpen ? "open" : ""}`}>
-                <li className="li rounded border-2 border-y-black border-x-black hover:border-x-indigo-500"><Link to="/">Home</Link></li>
-                <li className="li li-1 rounded border-2 border-y-black border-x-black hover:border-x-indigo-500"><Link to="/about-me">About Me</Link></li>
-                <li className="li li-1 rounded border-2 border-y-black border-x-black hover:border-x-indigo-500"><Link to="/my-projects">My Projects</Link>   </li>
-                <li className="li li-1 rounded border-2 border-y-black border-x-black hover:border-x-indigo-500"><Link to="/contact-me">Contact Me</Link></li>
+                <li className="li l1 rounded border-2 border-y-black border-x-black hover:border-x-indigo-500"><Link to="/">Home</Link></li>
+                <li className="li l rounded border-2 border-y-black border-x-black hover:border-x-indigo-500"><Link to="/about-me">About Me</Link></li>
+                <li className="li l rounded border-2 border-y-black border-x-black hover:border-x-indigo-500"><Link to="/my-projects">My Projects</Link>   </li>
+                <li className="li l rounded border-2 border-y-black border-x-black hover:border-x-indigo-500"><Link to="/contact-me">Contact Me</Link></li>
             </ul>
         </nav>
     );
